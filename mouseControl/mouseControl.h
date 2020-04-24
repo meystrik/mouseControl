96e2bc8b145a2864d39aa7f4b5e6d3cb6607fb87
C. Chris Meystrik <meystrik@meystrik.com>
2020-04-23 20:01:25 -0400

    Initial Commit

---
+++ mouseControl/mouseControl.h
@@ -0,0 +1,13 @@
+//
+//  mouseControl.h
+//  mouseControl
+//
+//  Created by Meystrik, Chris on 4/23/20.
+//  Copyright © 2020 C. Chris Meystrik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface mouseControl : NSObject
+
+@end
---
+++ mouseControl/mouseControl.m
@@ -0,0 +1,13 @@
+//
+//  mouseControl.m
+//  mouseControl
+//
+//  Created by Meystrik, Chris on 4/23/20.
+//  Copyright © 2020 C. Chris Meystrik. All rights reserved.
+//
+
+#import "mouseControl.h"
+
+@implementation mouseControl
+
+@end
