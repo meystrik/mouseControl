8251ea9f4f8b7ae76e8d5c2126eab3cd7c1c255e
C. Chris Meystrik <meystrik@meystrik.com>
2020-04-26 17:26:16 -0400

    Added the first of the left and right click events without knowledge of the actual screen resolutions they are dealing with, though the screen details are now included in the MAMouseControl object.

--- mouseControl/mouseControl.h
+++ mouseControl/mouseControl.h
@@ -10,23 +10,27 @@
 #import <ApplicationServices/ApplicationServices.h>
 #import <unistd.h>
 
+#define MAX_DISPLAYS            32
+
 @interface MADisplayDetails : NSObject
 {
-    size_t      pHigh;
-    size_t      pWide;
-    size_t      iHigh;
-    size_t      iWide;
-    bool        isBuiltIn;
-    bool        isMirrorSet;
-    bool        isMain;
-    double      rotation;
-    uint32_t    modelNum;
-    uint32_t    logicalNum;
+    CGDirectDisplayID   ID;
+    size_t              pHigh;
+    size_t              pWide;
+    size_t              mHigh;
+    size_t              mWide;
+    bool                isBuiltIn;
+    bool                isMirrorSet;
+    bool                isMain;
+    double              rotation;
+    uint32_t            modelNum;
+    uint32_t            logicalNum;
 }
+@property   CGDirectDisplayID ID;
 @property   size_t pHigh;
 @property   size_t pWide;
-@property   size_t iHigh;
-@property   size_t iWide;
+@property   size_t mHigh;
+@property   size_t mWide;
 @property   bool isBuiltIn;
 @property   bool isMirrorSet;
 @property   bool isMain;
@@ -37,5 +41,11 @@
 @end
 
 @interface MAMouseControl : NSObject
+{
+    uint32_t            nDisplays;
+    NSMutableArray      *displayDetails;
+    int                 delay;
+}
+@property int delay;
 
 @end
--- mouseControl/mouseControl.m
+++ mouseControl/mouseControl.m
@@ -10,11 +10,11 @@
 
 @implementation MADisplayDetails
 
-
+@synthesize ID;
 @synthesize pHigh;
 @synthesize pWide;
-@synthesize iHigh;
-@synthesize iWide;
+@synthesize mHigh;
+@synthesize mWide;
 @synthesize isBuiltIn;
 @synthesize isMirrorSet;
 @synthesize isMain;
@@ -22,12 +22,213 @@
 @synthesize modelNum;
 @synthesize logicalNum;
 
-
-
-
 @end
 
 @implementation MAMouseControl
 
-- (id) getDisplayList()
+@synthesize delay;
+
+- (int32_t) getDisplayList
+
+{
+    uint32_t count = 0;
+    int i;
+    MADisplayDetails *d;
+    CGSize s;
+    CGDirectDisplayID displayIDs[MAX_DISPLAYS];
+    
+    
+    memset(displayIDs, 0, sizeof(CGDirectDisplayID) * MAX_DISPLAYS );
+    CGGetOnlineDisplayList(nDisplays, displayIDs, &count);
+    
+    self->nDisplays = count;
+    [self->displayDetails removeAllObjects];
+    
+    for(i = 0; i < count; i++)  {
+        d = [[MADisplayDetails alloc]init];
+        
+        d.ID = displayIDs[i];
+        d.pWide = CGDisplayPixelsWide(displayIDs[i]);
+        d.pHigh = CGDisplayPixelsHigh(displayIDs[i]);
+        
+        s = CGDisplayScreenSize(displayIDs[i]);
+        d.mHigh = s.height;
+        d.mWide = s.width;
+        
+        d.isBuiltIn = CGDisplayIsBuiltin(displayIDs[i]);
+        d.isMirrorSet = CGDisplayIsInMirrorSet(displayIDs[i]);
+        d.isMain = CGDisplayIsMain(displayIDs[i]);
+        
+        d.rotation = CGDisplayRotation(displayIDs[i]);
+        d.modelNum = CGDisplayModelNumber(displayIDs[i]);
+        d.logicalNum = CGDisplayUnitNumber(displayIDs[i]);
+        
+        [self->displayDetails addObject:d];
+    }
+    
+    
+    return count;
+}
+
+
+- (int) mouseEvent: (CGPoint) p
+                   withEventType: (CGEventType) e
+{
+    
+    /*
+     *  Establish the Events
+     */
+    CGEventRef mouseevent = CGEventCreateMouseEvent(
+                                                    NULL,
+                                                    e,
+                                                    p,
+                                                    (CGMouseButton)e);
+    
+    /*
+     * Execute the Events defined above
+     */
+    CGEventPost(kCGHIDEventTap, mouseevent);
+    
+    
+    /*
+     * Release the Events
+     */
+    CFRelease(mouseevent);
+
+    return 0;
+}
+
+- (int) leftDown
+{
+    CGEventRef event = CGEventCreate(NULL);
+    CGPoint p = CGEventGetLocation(event);
+    CFRelease(event);
+    
+    return [self mouseEvent:p withEventType:kCGEventLeftMouseDown];
+}
+
+- (int) leftUp
+{
+    CGEventRef event = CGEventCreate(NULL);
+    CGPoint p = CGEventGetLocation(event);
+    CFRelease(event);
+    
+    return [self mouseEvent:p withEventType:kCGEventLeftMouseUp];
+}
+
+
+
+- (int) leftClick
+{
+    return [self leftClick: self.delay];
+}
+
+- (int) leftClick: (int) delay
+{
+    return [self leftClick: delay
+                    withDoubleClick: false];
+
+}
+
+- (int) leftClick:  (int) delay
+                    withDoubleClick: (bool) isDouble
+{
+    
+    CGEventRef event = CGEventCreate(NULL);
+    CGPoint p = CGEventGetLocation(event);
+    CFRelease(event);
+    
+    
+    return [self leftClick: delay
+                    withDoubleClick: isDouble
+                    onPointX: p.x
+                    andPointY: p.y];
+}
+
+- (int) leftClick: (int) delay
+                  withDoubleClick: (bool) isDouble
+                  onPointX: (int) x
+                  andPointY: (int) y
+{
+    CGPoint p;
+    int clicks = isDouble ? 2 : 1;
+    
+    p = CGPointMake(x, y);
+    
+    for(int i = 0; i < clicks; i++)  {
+        [self mouseEvent: p withEventType: kCGEventLeftMouseDown];
+        usleep(delay);
+        [self mouseEvent: p withEventType: kCGEventLeftMouseUp];
+    }
+    
+    return 0;
+}
+
+- (int) rightDown
+{
+    CGEventRef event = CGEventCreate(NULL);
+    CGPoint p = CGEventGetLocation(event);
+    CFRelease(event);
+    
+    return [self mouseEvent:p withEventType:kCGEventRightMouseDown];
+}
+
+- (int) RightUp
+{
+    CGEventRef event = CGEventCreate(NULL);
+    CGPoint p = CGEventGetLocation(event);
+    CFRelease(event);
+    
+    return [self mouseEvent:p withEventType:kCGEventRightMouseUp];
+}
+
+
+
+- (int) rightClick
+{
+    return [self rightClick: self.delay];
+}
+
+- (int) rightClick: (int) delay
+{
+    return [self rightClick: delay
+                    withDoubleClick: false];
+
+}
+
+- (int) rightClick:  (int) delay
+                    withDoubleClick: (bool) isDouble
+{
+    
+    CGEventRef event = CGEventCreate(NULL);
+    CGPoint p = CGEventGetLocation(event);
+    CFRelease(event);
+    
+    
+    return [self rightClick: delay
+                    withDoubleClick: isDouble
+                    onPointX: p.x
+                    andPointY: p.y];
+}
+
+- (int) rightClick: (int) delay
+                  withDoubleClick: (bool) isDouble
+                  onPointX: (int) x
+                  andPointY: (int) y
+{
+    CGPoint p;
+    int clicks = isDouble ? 2 : 1;
+    
+    p = CGPointMake(x, y);
+    
+    for(int i = 0; i < clicks; i++)  {
+        [self mouseEvent: p withEventType: kCGEventRightMouseDown];
+        usleep(delay);
+        [self mouseEvent: p withEventType: kCGEventRightMouseUp];
+    }
+    
+    return 0;
+}
+
+
 @end
